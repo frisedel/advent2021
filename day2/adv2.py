ddc2897057b609e4c48e09575c62b36459c25e53
Johan Dufberg <johan.dufberg@epiroc.com>
2021-12-02 15:28:40 +0100

    refactor

--- day2/adv2.py
+++ day2/adv2.py
@@ -2,15 +2,11 @@
 
 from typing import List
 
-def adv2_1(lines: List[str]):
+def adv2_1(path):
     depth = 0
     forward = 0
 
-    for line in lines:
-        commands = line.split()
-        command = commands[0]
-        amount = int(commands[1])
-
+    for command, amount in path:
         if command == "forward":
             forward += amount
         if command == "down":
@@ -22,16 +18,12 @@ def adv2_1(lines: List[str]):
     return forward * depth
 
 
-def adv2_2(lines: List[str]):
+def adv2_2(path):
     depth = 0
     forward = 0
     aim = 0
 
-    for line in lines:
-        commands = line.split()
-        command = commands[0]
-        amount = int(commands[1])
-
+    for command, amount in path:
         if command == "down":
             aim += amount
         if command == "up":
@@ -43,14 +35,20 @@ def adv2_2(lines: List[str]):
 
     return forward * depth
 
+
 def main():
     lines = []
     with open("path_commands.txt") as f:
         lines = f.readlines()
     f.close()
 
-    print("part 1:", adv2_1(lines))
-    print("part 2:", adv2_2(lines))
+    path_commands: List[(str, int)] = []
+    for line in lines:
+        commands = line.split()
+        path_commands.append((commands[0], int(commands[1])))
+
+    print("part 1:", adv2_1(path_commands))
+    print("part 2:", adv2_2(path_commands))
 
 
 if __name__ == '__main__':
