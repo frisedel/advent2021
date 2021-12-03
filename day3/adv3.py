934b3e31ca7accd55ffeb435479447cdf13f4385
Johan Dufberg <johan.dufberg@epiroc.com>
2021-12-03 23:03:28 +0100

    day 3 done and tests

--- day3/adv3.py
+++ day3/adv3.py
@@ -12,6 +12,7 @@ def get_bit_count(diag_codes: List[str]):
             bits_count[index] += int(bits[index])
     return bits_count
 
+
 def get_rates(diag_codes: List[str]):
     bits_count = get_bit_count(diag_codes)
 
@@ -30,17 +31,74 @@ def get_rates(diag_codes: List[str]):
 
     return gamma_rate, epsilon_rate
 
+
+def convert_binary(binary: str):
+    return int(binary, 2)
+
+
 def adv3_1(diag_codes: List[str]):
     gamma_rate, epislon_rate = get_rates(diag_codes)
-    gamma_value = int(gamma_rate, 2)
-    epsilon_value = int(epislon_rate, 2)
+    gamma_value = convert_binary(gamma_rate)
+    epsilon_value = convert_binary(epislon_rate)
     print("gamma:", gamma_value, "epsilon:", epsilon_value)
 
     return(gamma_value * epsilon_value)
 
 
+def calc_common(codes: List[str], index: int, gas: str):
+    one = 0
+    zero = 0
+
+    for code in codes:
+        bits = list(code)
+        if int(bits[index]) == 1:
+            one += 1
+        else:
+            zero += 1
+
+    if gas == "o2":
+        if one >= zero:
+            return 1
+        else:
+            return 0
+    if gas == "co2":
+        if one < zero:
+            return 1
+        else:
+            return 0
+
+
+def oxygen_generator_rating(diag_codes: List[str], start_index: int):
+    if len(diag_codes) == 1:
+        return convert_binary(diag_codes[0])
+    codes_for_next = []
+    most_common = calc_common(diag_codes, start_index, "o2")
+    for code in diag_codes:
+        bits = list(code)
+        if int(bits[start_index]) == most_common:
+            codes_for_next.append(code)
+    return oxygen_generator_rating(codes_for_next, start_index+1)
+
+
+def co2_scrubber_rating(diag_codes: List[str], start_index: int):
+    if len(diag_codes) == 1:
+        return convert_binary(diag_codes[0])
+    codes_for_next = []
+    most_common = calc_common(diag_codes, start_index, "co2")
+    for code in diag_codes:
+        bits = list(code)
+        if int(bits[start_index]) == most_common:
+            codes_for_next.append(code)
+    return co2_scrubber_rating(codes_for_next, start_index+1)
+
+
 def adv3_2(diag_codes: List[str]):
-    print("h")
+
+    oxygen_rating: str = oxygen_generator_rating(diag_codes, 0)
+    co2_rating: str = co2_scrubber_rating(diag_codes, 0)
+    print("o2:", oxygen_rating,"co2:", co2_rating)
+    return oxygen_rating * co2_rating
+
 
 def main():
     lines = []
--- test/test_adv3.py
+++ test/test_adv3.py
@@ -2,10 +2,51 @@
 
 import pytest
 
-from day3.adv3 import adv3_1
+from day3.adv3 import adv3_1, convert_binary, get_rates, adv3_2, calc_common, co2_scrubber_rating, oxygen_generator_rating
 
 def test_adv3_1():
     diagnostic_codes = ["00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"]
 
-    common = adv3_1(diagnostic_codes)
-    assert common == 198
+    power_consumption = adv3_1(diagnostic_codes)
+    assert power_consumption == 198
+
+def test_get_rates():
+    diagnostic_codes = ["00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"]
+
+    gamma, epislon = get_rates(diagnostic_codes)
+    assert gamma == "10110"
+    assert epislon == "01001"
+
+def test_convert_binary():
+    gamma = convert_binary("10110")
+    epislon = convert_binary("01001")
+
+    assert gamma == 22
+    assert epislon == 9
+
+def test_adv3_2():
+    diagnostic_codes = ["00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"]
+
+    life_support_rating = adv3_2(diagnostic_codes)
+    assert life_support_rating == 230
+
+def test_oxygen_generator_rating():
+    diagnostic_codes = ["00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"]
+
+    oxygen = oxygen_generator_rating(diagnostic_codes, 0)
+    assert oxygen == 23
+
+def test_co2_scrubber_rating():
+    diagnostic_codes = ["00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"]
+
+    oxygen = co2_scrubber_rating(diagnostic_codes, 0)
+    assert oxygen == 10
+
+def test_calc_common():
+    diagnostic_codes = ["00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"]
+
+    common_first_bit_o2 = calc_common(diagnostic_codes, 0, "o2")
+    assert common_first_bit_o2 == 1
+
+    common_first_bit_co2 = calc_common(diagnostic_codes, 0, "co2")
+    assert common_first_bit_co2 == 0
