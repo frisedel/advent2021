04527a6fa54a946f39863ed2faaffad11cc864b5
Johan Dufberg <johan.dufberg@epiroc.com>
2021-12-06 10:35:43 +0100

    day 5 - work flow done but no code

---
+++ day5/adv5.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+def create_vent_lists():
+    #remove diagonals from data
+    #create horizontal list
+    #create vertical list
+    #return lists
+    pass
+
+def mark_in_matrix():
+    #mark horizontal vents by + 1 in cell
+    #mark vertical vents the same way
+    pass
+
+def count_dangerous_vents():
+    #loop over matrix and count cells with value over 1
+    pass
+
+def adv5_1(vent_data):
+    #remove diagonals from data
+    #create 1000*1000 matrix of 0
+    #get horizontal and vertical lists
+    #mark vents
+    #count vents
+    #return vent value
+    pass
+
+def main():
+    #read alla lines
+    vent_data = []
+    print("part 1 - number of dangerous vents:", adv5_1(vent_data))
+    pass
+
+if __name__ == '__main__':
+    main()
