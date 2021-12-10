1ce1625868e074832d530d2c3beed71013298e87
Johan Dufberg <johan.dufberg@epiroc.com>
2021-12-10 13:54:41 +0100

    day7 - init commit

---
+++ day7/adv7.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def main():
+    # data = []
+    # with open(".txt") as f:
+
+    # f.close
+
+    # print("part 1 - number of lanternfish after 80 days:", adv6_1(lanterns))
+    # print("part 2 - number of lanternfish after 256 days:", adv6_2(lanterns))
+    pass
+
+if __name__ == '__main__':
+    main()
