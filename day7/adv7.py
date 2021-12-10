a20718f36190d9c7953e091d6ff587531012c966
Johan Dufberg <johan.dufberg@epiroc.com>
2021-12-10 19:59:40 +0100

    day7 - starting to think

--- day7/adv7.py
+++ day7/adv7.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python3
 
-from typing import List
+from typing import Dict, List
+import sys
+
+def adv7_1(crabs_data: List[int]) -> int:
+    crabs = crabs_data[:]
+    crabs.sort()
+    crab_max = max(crabs)
+    crab_min = min(crabs)
+
+    crabs_grouped: Dict[int, int] = {}
+
+    for crab in crabs:
+        if crab not in crabs_grouped.keys():
+            crabs_grouped[crab] = 1
+        else:
+            crabs_grouped[crab] += 1
+
+    min_fuel = sys.maxsize
+
+    # for i in range(crab_min, crab_max):
+    #     fuel_cost = 0
+    #     loop over every crab group in dict and calc the fuel from that position. muliply by amount for that group
+    #     for j in something
+
+    print(crabs_grouped)
 
 def main():
     crab_data = []
@@ -13,9 +37,10 @@ def main():
     for crab in crab_data:
         crabs.append(int(crab))
 
-    print(crabs)
+    print(len(crabs))
+    print(max(crabs))
 
-    # print("part 1 - number of lanternfish after 80 days:", adv6_1(lanterns))
+    print("part 1 - crabs to converge at:", adv7_1(crabs))
     # print("part 2 - number of lanternfish after 256 days:", adv6_2(lanterns))
     pass
 
