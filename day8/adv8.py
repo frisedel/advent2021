f5c450d23f9c87996437a309d97191d90f1170fb
Johan Dufberg <johan.dufberg@epiroc.com>
2021-12-13 08:29:50 +0100

    day8 - part 1 and tests

--- day8/adv8.py
+++ day8/adv8.py
@@ -1,10 +1,38 @@
 #!/usr/bin/env python3
 
-from typing import List
+from typing import Dict, List
+
+def find_numbers(segment_data: List[str]):
+    amount_numbers = {1:0, 4:0, 7:0, 8:0}
+    others = 0
+    for code in segment_data:
+        signal_values = code.split('|')[-1].strip().split()
+        for value in signal_values:
+            if len(value) == 2:
+                amount_numbers[1] += 1
+            elif len(value) == 4:
+                amount_numbers[4] += 1
+            elif len(value) == 3:
+                amount_numbers[7] += 1
+            elif len(value) == 7:
+                amount_numbers[8] += 1
+            else:
+                others += 1
+    return amount_numbers
+
+
+def count_numbers(numbers: Dict[int, int]):
+    appearances = 0
+    for number in numbers:
+        appearances += numbers[number]
+    return appearances
+
 
 def adv8_1(segment_data: List[str]):
-    for code in segment_data:
-        print(code)
+    amount_numbers = find_numbers(segment_data)
+    number_of_appearances = count_numbers(amount_numbers)
+    return number_of_appearances
+
 
 def main():
     data = []
---
+++ test/test_day8.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from day8.adv8 import count_numbers, find_numbers
+
+segment_text = [
+    "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
+    "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
+    "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
+    "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
+    "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea",
+    "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb",
+    "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe",
+    "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
+    "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
+    "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"
+]
+amount_numbers = {1: 8, 4: 6, 7: 5, 8: 7}
+
+def test_find_numbers():
+    numbers = find_numbers(segment_text)
+    print(numbers)
+    assert numbers == amount_numbers
+
+def test_count_numbers():
+    amount = count_numbers(amount_numbers)
+    assert amount == 26
