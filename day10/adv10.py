672853ce5ccd073529728a1eeae36725b8340f37
Johan Dufberg <johan.dufberg@epiroc.com>
2021-12-17 10:07:23 +0100

    day10 - started work

--- day10/adv10.py
+++ day10/adv10.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
-def adv10_1():
+from typing import List
+
+
+def adv10_1(syntax_data: List[str]):
+    for d in syntax_data:
+        print(d)
+
     pass
 
 def adv10_2():
@@ -12,7 +18,11 @@ def main():
         input_data = f.readlines()
     f.close
 
-    print("part 1 - :", adv10_1())
+    syntax_data = []
+    for line in input_data:
+        syntax_data.append(list(line.strip()))
+
+    print("part 1 - :", adv10_1(syntax_data))
     print("part 2 - :", adv10_2())
 
 if __name__ == '__main__':
