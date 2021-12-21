434e13fea2501743702f6df2f7fbe8144f9ec3b6
Johan Dufberg <johan.dufberg@epiroc.com>
2021-12-21 14:03:42 +0100

    day11 - starting part 1

--- day11/adv11.py
+++ day11/adv11.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python3
 
-from typing import List
+from typing import Dict, List
 
-def adv11_1():
-    pass
+def create_grid(octopus_data: List[List[int]]):
+    oct_grid: List[List[Dict[str, int]]] = []
+    for line in octopus_data:
+        oct_line: List[Dict[str, int]] = []
+        for value in line:
+            oct_line.append({"value": value, "last flash": 0, "flash num": 0})
+        oct_grid.append(oct_line)
+    return oct_grid
+
+
+def adv11_1(octopus_data: List[List[int]]):
+    octopus_grid = create_grid(octopus_data)
+    """
+        1. iterate over grid adding 1 untill above 9. ie when 10 set to 0
+        2. make turning at 10 add 1 to all adjacent octopuses. use logic from day 9 but add diagonals as well
+            a flash turns octopus to 0, but can be counted up again by neighbours the same round
+            next iteration does not mean it is 0 then
+    """
 
 
 def adv11_2():
@@ -11,12 +27,16 @@ def adv11_2():
 
 
 def main():
-    octopus_data = []
+    data = []
     with open("octopus_grid.txt") as f:
-        octopus_data = f.readlines()
+        data = f.readlines()
     f.close
 
-    print("part 1 - :", adv11_1())
+    octopus_data: List[List[int]] = []
+    for line in data:
+        octopus_data.append([int(i) for i in list(line.strip())])
+
+    print("part 1 - :", adv11_1(octopus_data))
     print("part 2 - :", adv11_2())
 
 if __name__ == '__main__':
---
+++ test/test_day11.py
@@ -0,0 +1,30 @@
+from day11.adv11 import adv11_1, create_grid
+
+test_grid = [
+    [5, 4, 8 ,3 ,1 ,4 ,3 ,2 ,2 ,3],
+    [2, 7, 4 ,5 ,8 ,5 ,4 ,7 ,1 ,1],
+    [5, 2, 6 ,4 ,5 ,5 ,6 ,1 ,7 ,3],
+    [6, 1, 4 ,1 ,3 ,3 ,6 ,1 ,4 ,6],
+    [6, 3, 5 ,7 ,3 ,8 ,5 ,4 ,7 ,8],
+    [4, 1, 6 ,7 ,5 ,2 ,4 ,6 ,4 ,5],
+    [2, 1, 7 ,6 ,8 ,4 ,1 ,7 ,2 ,1],
+    [6, 8, 8 ,2 ,8 ,8 ,1 ,1 ,3 ,4],
+    [4, 8, 4 ,6 ,8 ,4 ,8 ,5 ,5 ,4],
+    [5, 2, 8 ,3 ,7 ,5 ,1 ,5 ,2 ,6]
+]
+
+test_grid_small = [
+    [1, 2],
+    [3, 4]
+]
+
+def test_make_grid():
+    small_grid = create_grid(test_grid_small)
+    assert small_grid == [
+        [{"value": 1, "last flash": 0, "flash num": 0}, {"value": 2, "last flash": 0, "flash num": 0}],
+        [{"value": 3, "last flash": 0, "flash num": 0}, {"value": 4, "last flash": 0, "flash num": 0}]
+    ]
+
+# def test_adv11_1():
+#     flashes = adv11_1(test_grid)
+#     assert flashes == 1656
