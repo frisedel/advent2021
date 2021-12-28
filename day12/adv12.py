d7aa98620df1edc9529ed9993ee205b40338e9ca
Johan Dufberg <johan.dufberg@epiroc.com>
2021-12-28 22:34:05 +0100

    day12 - removed trimming since it was not needed, even though it worked as intended

--- day12/adv12.py
+++ day12/adv12.py
@@ -2,11 +2,11 @@
 
 from typing import Dict, List
 from collections import Counter
-from copy import deepcopy
 
 def visited_a_small_twice(path: List[str]) -> bool:
     small_caves = [cave for cave in path if cave.islower()]
     return len([cave for cave, v in Counter(small_caves).items() if v > 1]) > 0
+
 
 def navigate_caves(cave_graph: Dict[str, List[str]], paths: List[List[str]], path: List[str], cave: str, use_double_visit: bool = False):
     path.append(cave)
@@ -25,25 +25,9 @@ def navigate_caves(cave_graph: Dict[str, List[str]], paths: List[List[str]], pat
             navigate_caves(cave_graph, paths, path[:], neighbour, use_double_visit)
 
 
-def trim_graph(cave_graph: Dict[str, List[str]]):
-    to_remove: List[str] = []
-    for node in cave_graph:
-        if len(cave_graph[node]) == 0:
-            to_remove.append(node)
-        if node != 'start' and node != 'end' and len(cave_graph[node]) == 1 and cave_graph[node][0].islower():
-            to_remove.append(node)
-    for dead_end in to_remove:
-        del cave_graph[dead_end]
-        for node in cave_graph:
-            if dead_end in cave_graph[node]:
-                cave_graph[node].remove(dead_end)
-
-
 def adv12_1(cave_graph: Dict[str, List[str]]):
-    trimmed = deepcopy(cave_graph)
-    trim_graph(trimmed)
     paths: List[List[str]] = []
-    navigate_caves(trimmed, paths, [], "start")
+    navigate_caves(cave_graph, paths, [], "start")
     return len(paths)
 
 
@@ -69,8 +53,6 @@ def main():
 
         cave_graph[node_conection[1]].append(node_conection[0])
         cave_graph[node_conection[0]].append(node_conection[1])
-
-
 
     print("part 1 - Number of paths to end:", adv12_1(cave_graph))
     print("part 2 - Number of paths to end with one double visit:", adv12_2(cave_graph))
--- test/test_day12.py
+++ test/test_day12.py
@@ -1,8 +1,6 @@
-from copy import deepcopy
-from day12.adv12 import adv12_1, adv12_2, navigate_caves, trim_graph
+from day12.adv12 import adv12_1, adv12_2, navigate_caves, visited_a_small_twice
 
 test_caves_small = {'start': ['A', 'b'], 'A': ['start', 'b', 'c', 'end'], 'b': ['start', 'A', 'd', 'end'], 'c': ['A'], 'd': ['b']}
-test_caves_small_trimmed = {'start': ['A', 'b'], 'A': ['start', 'b', 'c', 'end'], 'b': ['start', 'A', 'end'], 'c': ['A']}
 
 test_paths_small = [
     ['start', 'A', 'b', 'A', 'c', 'A', 'end'],
@@ -17,20 +15,24 @@ test_paths_small = [
     ['start', 'b', 'end']
 ]
 
-def test_trim_graph():
-    local = deepcopy(test_caves_small)
-    trim_graph(local)
-    assert local == test_caves_small_trimmed
+test_path_double_small = ['start', 'A', 'b', 'A', 'b', 'A', 'c', 'A', 'end']
+
 
 def test_navigate_caves():
     paths = []
-    navigate_caves(test_caves_small_trimmed, paths, [], "start")
+    navigate_caves(test_caves_small, paths, [], "start")
     assert paths == test_paths_small
 
+def test_visit_small_twice():
+    has_not = visited_a_small_twice(test_paths_small[0])
+    assert has_not == False
+    has = visited_a_small_twice(test_path_double_small)
+    assert has == True
+
 def test_adv12_1():
-    number_of_paths = adv12_1(test_caves_small_trimmed)
+    number_of_paths = adv12_1(test_caves_small)
     assert number_of_paths == len(test_paths_small)
 
 def test_adv12_2():
-    number_of_paths = adv12_2(test_caves_small_trimmed)
+    number_of_paths = adv12_2(test_caves_small)
     assert number_of_paths == 36
