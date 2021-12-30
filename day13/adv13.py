0011e1ad609af96ef4180d23c47ce84f3043a81b
Johan Dufberg <johan.dufberg@epiroc.com>
2021-12-30 20:29:40 +0100

    day13 - part1 done with ease. :D

---
+++ day13/adv13.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+from typing import List, Tuple
+from itertools import zip_longest
+
+def fold_paper(paper: List[List[int]], fold: Tuple[str, int]):
+    if fold[0] == 'x':
+        return fold_x(paper, fold[1])
+    else:
+        return fold_y(paper, fold)
+
+def fold_x(paper: List[List[int]], fold_at: int):
+    folded_paper: List[List[int]] = []
+    for line in paper:
+        first = line[:fold_at]
+        first.reverse()
+        second = line[fold_at+1:]
+        new_line = list(map(sum, zip_longest(first, second, fillvalue=0)))
+        new_line.reverse()
+        folded_paper.append(new_line)
+    return folded_paper
+
+
+def fold_y(paper: List[List[int]], fold_at: int):
+    pass
+
+
+def count_dots(paper: List[List[int]]) -> int:
+    total = 0
+    for line in paper:
+        total += len(line) - line.count(0)
+    return total
+
+
+def adv13_1(paper: List[List[int]], folds: List[Tuple[str, int]]):
+    folded_paper = fold_paper(paper, folds[0])
+    return count_dots(folded_paper)
+
+
+def adv13_2():
+    pass
+
+
+def construct_coordinates(paper_data: List[str]):
+    return list(map(eval, paper_data))
+
+
+def construct_paper(coordinates: List[Tuple[int, int]]) -> List[List[int]]:
+    x_max = 0
+    y_max = 0
+    for coord in coordinates:
+        if coord[0] > x_max:
+            x_max = coord[0]
+        if coord[1] > y_max:
+            y_max = coord[1]
+
+    paper = [[0 for x in range(x_max+1)] for y in range(y_max+1)]
+
+    for x,y in coordinates:
+        paper[y][x] +=1
+
+    return paper
+
+
+def extraxt_folds(instructions: List[str]) -> List[Tuple[str, int]]:
+    folds = []
+    for line in instructions:
+        f = line.split()[-1].split('=')
+        folds.append((f[0], int(f[1])))
+    return folds
+
+
+def main():
+    paper_data = []
+    with open("paper_data.txt") as f:
+        paper_data = f.readlines()
+    f.close
+
+    dot_coordinates: List[Tuple[int, int]] = construct_coordinates(paper_data)
+    paper = construct_paper(dot_coordinates)
+
+    folding_data = []
+    with open("fold_instructions.txt") as f:
+        folding_data = f.readlines()
+    f.close
+
+    folds = extraxt_folds(folding_data)
+
+    print("part 1 - Number of visible dots after first fold:", adv13_1(paper, folds))
+    print("part 2 - :", adv13_2())
+
+if __name__ == '__main__':
+    main()
---
+++ test/test_day13.py
@@ -0,0 +1,64 @@
+from day13.adv13 import construct_coordinates, construct_paper, count_dots, extraxt_folds, fold_paper, fold_x
+
+test_coordinates_data = ['6,10', '0,14', '9,10', '0,3', '10,4', '4,11', '6,0', '6,12', '4,1', '0,13', '10,12', '3,4', '3,0', '8,4', '1,10', '2,14', '8,10', '9,0']
+test_coordinates = [(6, 10), (0, 14), (9, 10), (0, 3), (10, 4), (4, 11), (6, 0), (6, 12), (4, 1), (0, 13), (10, 12), (3, 4), (3, 0), (8, 4), (1, 10), (2, 14), (8, 10), (9, 0)]
+test_paper_start = [
+    [0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+test_paper_after_first = [
+    [1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0],
+    [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 0, 1, 0, 2, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+test_final_paper = [
+    [1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 2, 2, 1, 1],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+]
+
+test_fold_data = ['fold along y=7', 'fold along x=5']
+test_folds = [('y', 7), ('x', 5)]
+
+def test_construct_coordinates():
+    coordinates = construct_coordinates(test_coordinates_data)
+    assert coordinates == test_coordinates
+
+def test_construct_paper():
+    paper = construct_paper(test_coordinates)
+    assert paper == test_paper_start
+
+def test_extraxt_folds():
+    folds = extraxt_folds(test_fold_data)
+    assert folds == test_folds
+
+def test_count_dots():
+    number_dots = count_dots(test_final_paper)
+    assert number_dots == 16
+
+def test_fold_x():
+    after = fold_x(test_paper_after_first, 5)
+    assert after == test_final_paper
