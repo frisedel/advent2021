e33ae3a681ded3d8d9ecd8ec2beaa7d2fcf9d07c
Johan Dufberg <johan.dufberg@epiroc.com>
2022-01-19 16:34:30 +0100

    day16 - beginning to write code.

--- day16/adv16.py
+++ day16/adv16.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-def adv16_1(binary_message: str):
+def adv16_1(base_message: str):
     """
     while binary has any data, pop what is needed from the front and analyze
         first read header - 6 bits that is version and type id
@@ -22,10 +22,29 @@ def adv16_1(binary_message: str):
     read header, first 3 bits is version add that numbver to total
     check version
         if 4 - read and count bits untill the group of 5 starts with 0. read bits so that message is multiple of 4
-        if 0 - read next 15 bits as number, and read all thouse to total of message then add zeros as needed
-        if 1 - read next 11 bits as number, and read all thouse to total of message then add zeros as needed
+        if 0 - read next 15 bits as number, this is the lenght of the next part
+        if 1 - read next 11 bits as number, this is the lenght of the next part
+    read correct amount of bits as spec i previous step.
     next header starts now, repeat
     """
+    binary_message = base_message[:]
+    version_total = 0
+    while len(binary_message) > 0:
+        head = binary_message[:6]
+        version = int(head[:3], 2)
+        version_total += version
+        type_id = int(head[3:], 2)
+        binary_message = binary_message[6:]
+        if type_id == 4:
+            # read_literal() when value is needed
+            # for now, just skip correct amount of bits
+            pass
+        else:
+            # read_operator() when doing operations (this should be recusive for operators in operators)
+            # for now, just skip correct amount of bits
+            pass
+    return version_total
+
 
 def adv16_2():
     pass
@@ -37,11 +56,11 @@ def main():
         data = f.readlines()
     f.close
 
-    binary = ''
+    binary_string = ''
     for hex in data[0]:
-        binary += str("{0:04b}".format(int(hex, 16)))
+        binary_string += str("{0:04b}".format(int(hex, 16)))
 
-    print("part 1 - :", adv16_1(binary))
+    print("part 1 - :", adv16_1(binary_string))
     print("part 2 - :", adv16_2())
 
 if __name__ == '__main__':
