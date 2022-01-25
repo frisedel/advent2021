4a2348ad1f35ba8779a174e68914a5b9986b532c
frisedel <frisedel@gmail.com>
2022-01-25 10:44:35 +0100

    day16 - more thoughts. still no clue as how to solve this

--- day16/adv16.py
+++ day16/adv16.py
@@ -1,4 +1,61 @@
 #!/usr/bin/env python3
+from typing import Tuple
+
+def cut_str(transmission: str, cut_at: int) -> Tuple[str, str]:
+    return transmission[:cut_at], transmission[cut_at:]
+
+
+def cut_str_to_int(transmission: str, cut_at: int) -> Tuple[int, str]:
+    val, transmission = cut_str(transmission, cut_at)
+    return int(val, 2), transmission
+
+
+def read_litteral(transmission: str) -> None:
+    last_block = False
+    # this should process the value for literal, but that is not needed for part 1
+    # litteral_string = ''
+
+    """
+    add functionality to know total length of litteral so that zeros are read to make the litteral the correct length
+    """
+
+    while not last_block:
+        block, transmission = cut_str(transmission, 5)
+        if block[0] == "0":
+            last_block = True
+            # add to litteral_string
+        # else:
+        #     add to litteral_string
+    # convert litteral_string to int and return
+
+
+def read_operator(transmission: str, version_total: int):
+    operator_lenght_type, transmission = cut_str(transmission, 1)
+    if operator_lenght_type == "0":
+        message_lenght, transmission = cut_str_to_int(transmission, 15)
+        operator_payload, transmission = cut_str(transmission, message_lenght)
+        return sum_version(operator_payload, version_total)
+    else:
+        number_sub_packs, transmission = cut_str_to_int(transmission, 11)
+        acc_total = 0
+        for i in range(number_sub_packs):
+            version_total += sum_version()
+
+
+def sum_version(transmission: str, version_total: int):
+    if len(transmission) == 0:
+        return version_total
+    
+    header, transmission = cut_str(transmission, 6)
+    version_total += int(header[:3], 2)
+    type_id = int(header[3:6], 2)
+
+    if type_id == 4:
+        read_litteral(transmission)
+    else:
+        read_operator(transmission, version_total)
+    sum_version(transmission, version_total)
+
 
 def adv16_1(base_message: str):
     """
