0faa4964fd3bc12f25bf75d423a982026a3f07ef
Johan Dufberg <johan.dufberg@epiroc.com>
2022-02-15 09:45:09 +0100

    day18 - started to make numbers

--- 2021/day18/adv18.py
+++ 2021/day18/adv18.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
-def adv18_1():
+from ast import literal_eval
+from typing import List
+
+class Sailfish_number:
+    def __init__(self, left: int or List[int], right: int or List[int]):
+        self.left: int or Sailfish_number = None
+        self.right: int or Sailfish_number = None
+        if type(left) == list:
+            self.left = Sailfish_number(left[0], left[1])
+        else:
+            self.left = left
+        if type(right) == list:
+            self.right = Sailfish_number(right[0], right[1])
+        else:
+            self.right = right
+        print(self.left, self.right)
+
+
+def adv18_1(numbers: List[int or Sailfish_number]):
     pass
 
 
@@ -14,7 +32,12 @@ def main():
         data = f.readlines()
     f.close
 
-    print("part 1 - :", adv18_1())
+    numbers = []
+    for line in data:
+        sfn = literal_eval(line)
+        numbers.append(Sailfish_number(sfn[0], sfn[1]))
+
+    print("part 1 - :", adv18_1(numbers))
     print("part 2 - :", adv18_2())
 
 if __name__ == '__main__':
