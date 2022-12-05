db3296538a50db9cd1ab8761bd85fe508cc409c6
Johan Dufberg <johan.dufberg@epiroc.com>
2022-12-05 10:30:59 +0100

    2022 - day 1 both parts done and tests work

---
+++ 2022/day1/adv_1.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+
+def sum_calories(elf_calories: List[List[int]]) -> List[int]:
+    compressed_calories = []
+    for elf in elf_calories:
+        compressed_calories.append(sum(elf))
+    return compressed_calories
+
+
+def convert_calorie_data(calorie_data: List[str]) -> List[List[int]]:
+    size = len(calorie_data)
+    idx_list = [idx + 1 for idx, val in enumerate(calorie_data) if val == '\n']
+    split_data = [calorie_data[i: j] for i, j in zip(
+        [0] + idx_list, idx_list + ([size] if idx_list[-1] != size else []))]
+
+    elf_calories: List[List[int]] = []
+    for data in split_data:
+        elf_snacks: List[int] = []
+        for snack in data:
+            if snack != '\n':
+                elf_snacks.append(int(snack.strip()))
+        elf_calories.append(elf_snacks)
+
+    return elf_calories
+
+
+def adv1_1(elf_calories: List[int]) -> int:
+    return max(elf_calories)
+
+
+def adv1_2(elf_calories: List[int]) -> int:
+    elf_calories.sort(reverse=True)
+    top_three = elf_calories[:3]
+    return sum(top_three)
+
+
+def main():
+
+    lines = []
+    with open("calories.txt") as f:
+        lines = f.readlines()
+    f.close()
+
+    elf_calories = convert_calorie_data(lines)
+    compressed_calories = sum_calories(elf_calories)
+
+    print("part 1 - max calories carried by one elf: ",
+          adv1_1(compressed_calories))
+    print("part 2 - max calories carried by three elfs: ",
+          adv1_2(compressed_calories))
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2022/test/test_day1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from day1.adv_1 import adv1_1, adv1_2, convert_calorie_data, sum_calories
+from test_input.test_data_day1 import test_calories
+
+converted_calories = [[1000, 2000, 3000], [4000], [5000, 6000],
+                      [7000, 8000, 9000], [10000]]
+
+summed_calories = [6000, 4000, 11000, 24000, 10000]
+
+
+def test_convert_calories():
+    converted = convert_calorie_data(test_calories)
+    assert converted == converted_calories
+
+
+def test_sum_calories():
+    summed = sum_calories(converted_calories)
+    assert summed == summed_calories
+
+
+def test_adv1_1():
+    max_calories = adv1_1(summed_calories)
+    assert max_calories == 24000
+
+
+def test_adv1_2():
+    top_tree = adv1_2(summed_calories)
+    assert top_tree == 45000
