d8bfa4c36d2eb81a1aca09cb3f372a2ddec2e9d5
frisedel <frisedel@gmail.com>
2022-12-16 20:23:23 +0100

    2022 - day 9 part 1, first draft of logic

--- 2022/day9/adv_9.py
+++ 2022/day9/adv_9.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 
-def adv9_1():
+from typing import List
+
+
+def adv9_1(rope_movements: List[str]):
+    head = (0,0)
+    tail = (0,0)
+    tail_positions = []
+    # read moves
+        # move head
+        # move tail to cach up, diagonaly first if needed
+        # add all pos to list
+    # count tail pos with set()
     return
 
 
@@ -15,7 +26,7 @@ def main():
         rope_movements = f.read().splitlines()
     f.close()
 
-    print("part 1 - :", adv9_1())
+    print("part 1 - :", adv9_1(rope_movements))
     print("part 2 - :", adv9_2())
 
 if __name__ == '__main__':
