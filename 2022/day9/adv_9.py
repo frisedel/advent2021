2d9c279c6db616358443a3e3bb726a1c3a1598da
Johan Dufberg <johan.dufberg@epiroc.com>
2022-12-22 08:58:26 +0100

    2022 - day 9 part 2 done, tests done everything works now

--- 2022/day9/adv_9.py
+++ 2022/day9/adv_9.py
@@ -49,29 +49,31 @@ def adv9_1(rope_movements: List[Movement]) -> int:
     return len(set(tail_positions))
 
 
-def move_rope(head: Dict[str, int], knots: List[Dict[str, int]], knot_positions: List[List[str]]):
-    # move first knot as before but in steps
-    return
+def move_rope(move: Movement, knots: List[Dict[str, int]], tail_positions: List[str]) -> None:
+    for _ in range(move.amount):
+        if move.direction == "R":
+            knots[0]["x"] += 1
+        elif move.direction == "L":
+            knots[0]["x"] -= 1
+        elif move.direction == "U":
+            knots[0]["y"] += 1
+        elif move.direction == "D":
+            knots[0]["y"] -= 1
+        for prev, cur in zip(knots, knots[1:]):
+            while (abs(cur["x"] - prev["x"]) > 1 or abs(cur["y"] - prev["y"]) > 1):
+                cur["x"] += (cur["x"] < prev["x"]) - (prev["x"] < cur["x"])
+                cur["y"] += (cur["y"] < prev["y"]) - (prev["y"] < cur["y"])
+        tail_positions.append(f'{knots[-1]["x"]} {knots[-1]["y"]}')
 
 
-def adv9_2(rope_movements: List[str]) -> int:
-    head = {"x": 0, "y": 0}
-    knots = [{"x": 0, "y": 0} for _ in range(9)]
-    knots_positions = [["0 0"] for _ in range(9)]
+def adv9_2(rope_movements: List[Movement]) -> int:
+    knots = [{"x": 0, "y": 0} for _ in range(10)]
+    tail_positions = ["0 0"]
 
     for move in rope_movements:
-        move_head(head, move)
-        move_tail(head, knots[0], knots_positions[0])
-        move_tail(knots[0], knots[1], knots_positions[1])
-        move_tail(knots[1], knots[2], knots_positions[2])
-        move_tail(knots[2], knots[3], knots_positions[3])
-        move_tail(knots[3], knots[4], knots_positions[4])
-        move_tail(knots[4], knots[5], knots_positions[5])
-        move_tail(knots[5], knots[6], knots_positions[6])
-        move_tail(knots[6], knots[7], knots_positions[7])
-        move_tail(knots[7], knots[8], knots_positions[8])
+        move_rope(move, knots, tail_positions)
 
-    return len(set(knots_positions[-1]))
+    return len(set(tail_positions))
 
 
 def extract_movements(rope_data: List[str]) -> List[Movement]:
@@ -92,7 +94,7 @@ def main():
     rope_movements = extract_movements(rope_data)
 
     print("part 1 - Number of unique tail positions:", adv9_1(rope_movements))
-    print("part 2 - :", adv9_2(rope_movements))
+    print("part 2 - Number of unique tail positions for snapped rope:", adv9_2(rope_movements))
 
 if __name__ == '__main__':
     main()
--- 2022/test/test_day9.py
+++ 2022/test/test_day9.py
@@ -1,10 +1,37 @@
-from day9.adv_9 import adv9_1, adv9_2, extract_movements
+from day9.adv_9 import Movement, adv9_1, adv9_2, extract_movements, move_head, move_rope, move_tail
 from test_input.test_data_day9 import test_rope_moves, test_rope_moves_large
+
+def test_move_head():
+    test_head = {"x": 0, "y": 0}
+    test_move = Movement("R", 4)
+    move_head(test_head, test_move)
+    assert test_head == {"x": 4, "y": 0}
+
+def test_move_tail():
+    test_head = {"x": 4, "y": 0}
+    test_tail = {"x": 0, "y": 0}
+    test_tail_positions = ["0 0"]
+    move_tail(test_head, test_tail, test_tail_positions)
+    assert test_tail == {"x": 3, "y": 0}
+    assert test_tail_positions == ["0 0", "1 0", "2 0", "3 0"]
 
 def test_adv9_1():
     test_moves = extract_movements(test_rope_moves)
     count = adv9_1(test_moves)
     assert count == 13
+
+def test_move_rope():
+    test_head = {"x": 0, "y": 0}
+    test_knot = {"x": 0, "y": 0}
+    test_tail = {"x": 0, "y": 0}
+    test_tail_positions = []
+    test_move = Movement("R", 4)
+    move_rope(test_move, [test_head, test_knot, test_tail], test_tail_positions)
+    assert test_head == {"x": 4, "y": 0}
+    assert test_tail == {"x": 2, "y": 0}
+    test_set = set(test_tail_positions)
+    assert len(test_set) == 3
+    assert "0 0" in test_set and "1 0" in test_set and "2 0" in test_set
 
 def test_adv9_2():
     test_moves = extract_movements(test_rope_moves)
